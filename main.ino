7ced69deeb498da203842840ccd97ee72f69e608
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-08-02 12:05:23 +0800

    main.ino

---
+++ main.ino
@@ -0,0 +1,24 @@
+#include "LoaderV1.h"
+
+#define button 7
+
+LoaderV1 loader(9, 2, 100);
+bool flag = 0;
+
+void setup() {
+  pinMode(button, INPUT_PULLUP);
+  digitalWrite(button, HIGH);
+}
+
+void loop() {
+  loader.update();
+  if (digitalRead(button) == 0 && flag == 0) {
+     loader.load(60, 2, 30);
+    flag = 1;
+  } else if (digitalRead(button) == 1 && flag == 1) {
+      flag = 0;
+    }
+  Serial.print("Loader status: ");
+  
+  Serial.println(loader.isloaded());
+}
