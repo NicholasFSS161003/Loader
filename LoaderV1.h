370708e4ee70ea5b755d4f7c1e3b399924eb0d0a
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-08-02 14:56:38 +0800

    LoaderV1.h

--- LoaderV1.h
+++ LoaderV1.h
@@ -7,16 +7,17 @@ private:
   uint8_t _init_value;
   uint8_t _servo_pin;
   uint8_t _buttonPin; 
-  uint8_t _counter = 0;
+  uint8_t _counter = 0,_counter2=0;
   bool _isloaded = 0;
   bool _flag = 0;
   uint8_t _rdu_dist = 0;
-  uint8_t _edge_load_dist;
+  uint8_t _edge_load_dist, _init_dist;
+  
 public:
-  LoaderV1(uint8_t servo_pin, uint8_t init_value, uint8_t buttonPin);
-  void load(uint8_t init_dist, uint8_t rdu,uint8_t rdu_limit);
+  LoaderV1(uint8_t servo_pin, uint8_t buttonPin, uint8_t init_value);
+  void load(uint8_t edge_load_dist,uint8_t change,uint8_t counter2_limit);
   bool isloaded();
   void reset_counter();
   void detect_LoadDist();
-  void update();
+  void update(uint8_t init_dist, uint8_t rdu, uint8_t rdu_limit);
 };
