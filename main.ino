f3513281d6d9ca4bdbafe2aafd1771a8b3d2edc2
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-08-02 16:32:13 +0800

    main.ino

--- main.ino
+++ main.ino
@@ -1,26 +1,28 @@
 #include "LoaderV1.h"
+#include <Arduino.h>
 
-#define button 7
+#define button 2
 
-LoaderV1 loader(9, 2, 100);
+LoaderV1 loader(9, 7, 100);
 bool flag = 0;
-
 void setup() {
   Serial.begin(9600);
+  loader.attach(9);
+  loader.reset();
   pinMode(button, INPUT_PULLUP);
   digitalWrite(button, HIGH);
 }
 
 void loop() {
-  if(digitalRead(button)==0 && flag ==0){
+    loader.load(90,2,30);
+ if(digitalRead(button)==0 && flag ==0){
   loader.update(60,2,30);
+
    flag = 1;
   }else if(digitalRead(button)==1 && flag ==1){
     flag = 0;
   }
-
-  loader.load(90,2,30);
-  
-  Serial.print("Loader status: ");
+    Serial.print("Loader status1:  ");
   Serial.println(loader.isloaded());
+
 }
