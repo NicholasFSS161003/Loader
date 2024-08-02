52a01b42efecd96370c915181927fae3cca58261
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-08-02 16:33:26 +0800

    LoaderV1.cpp

--- LoaderV1.cpp
+++ LoaderV1.cpp
@@ -1,19 +1,14 @@
-#pragma once
 #include "LoaderV1.h"
 
-Servo Servo;
 
 LoaderV1::LoaderV1(uint8_t servo_pin, uint8_t buttonPin, uint8_t init_value)
   : _servo_pin(servo_pin), _buttonPin(buttonPin), _init_value(init_value), _edge_load_dist(init_value) {
-  Servo.attach(_servo_pin);
-  Servo.write(_init_value);
   pinMode(_buttonPin, INPUT_PULLUP);
-  digitalWrite(_buttonPin, HIGH);
 }
 
 void LoaderV1::load(uint8_t edge_load_dist,uint8_t change,uint8_t counter2_limit) {
   _isloaded = 0;
-  Servo.write(edge_load_dist-_counter2);
+  setServo(edge_load_dist-_counter2);
   _counter2 += change;
   if(_counter2 >= counter2_limit)
     _counter2 = counter2_limit;
@@ -23,9 +18,9 @@ bool LoaderV1::isloaded() {
   return _isloaded;
 }
 
-void LoaderV1::reset_counter() {
+void LoaderV1::reset() {
   _counter = 0;
-  Servo.write(_init_value);
+  setServo(_init_value);
   _rdu_dist = _init_value;
 }
 
@@ -36,9 +31,9 @@ void LoaderV1::detect_LoadDist(){
 }
 
 void LoaderV1::update(uint8_t init_dist, uint8_t rdu, uint8_t rdu_limit) {
-  if (_isloaded == 0) {
+  if(_isloaded ==0) {
     if (digitalRead(_buttonPin) == 0 && _flag == 0) {
-      Servo.write(init_dist - _rdu_dist);
+      setServo(init_dist - _rdu_dist);
       _rdu_dist += rdu;
       ++_counter;
       _isloaded = 1;
@@ -48,5 +43,10 @@ void LoaderV1::update(uint8_t init_dist, uint8_t rdu, uint8_t rdu_limit) {
     if (_rdu_dist >= rdu_limit) {
       _counter = rdu;
     }
-   }
+  }
+}
+
+
+void LoaderV1::setServo(uint8_t angle){
+  write(angle);
 }
