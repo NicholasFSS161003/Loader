94a4a17eb2fe42cddade33262c5d4a21b595700c
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-08-06 15:52:25 +0800

    Update LoaderV1.h
    Signed-off-by: NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>

--- LoaderV1.h
+++ LoaderV1.h
@@ -9,22 +9,19 @@ private:
   uint8_t _init_value;
   uint8_t _servo_pin;
   uint8_t _buttonPin; 
-  uint8_t _counter = 0,_counter2=0;
-  bool _isloaded = 1;
+  bool _isloaded = 0;
   bool _LoadFlag = 0;
-  uint8_t _rdu_dist = 0;
+  uint8_t _rdu_dist,_rdu2_dist;
   uint8_t _edge_load_dist, _init_dist;
-  uint8_t _rdu,_rdu_limit,_change;  
+  double _rdu,_rdu2,_change;  
   double _frontVal;
   double _frontAng = _edge_load_dist;
-  unsigned long lastDebounceTime = millis();
-
+  unsigned long _currentDebounceTime;
 public:
   LoaderV1(uint8_t servo_pin, uint8_t buttonPin, uint8_t init_value);
-  void load(uint8_t edge_load_dist,uint8_t _rdu,uint8_t rdu_dist,double _frontVal);
+  void load(uint8_t edge_load_dist,double frontVal,double rdu,uint8_t rdu_dist);
   bool isloaded();
   void setServo(uint8_t angle);
   void reset();
-  int detect_LoadDist();
   void update();
 };
