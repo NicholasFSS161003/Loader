ba8e9b9bccc8caaf9d4d538c8968d723307a6219
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-08-06 15:51:56 +0800

    Update LoaderV1.cpp
    Signed-off-by: NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>

--- LoaderV1.cpp
+++ LoaderV1.cpp
@@ -1,63 +1,48 @@
 #include "LoaderV1.h"
-
 LoaderV1::LoaderV1(uint8_t servo_pin, uint8_t buttonPin, uint8_t init_value)
   : _servo_pin(servo_pin), _buttonPin(buttonPin), _init_value(init_value) {
   pinMode(_buttonPin, INPUT_PULLUP);
   setServo(_init_value);
 }
 
-void LoaderV1::load(uint8_t edge_load_dist,uint8_t rdu,uint8_t rdu_dist,double frontVal){
+void LoaderV1::load(uint8_t edge_load_dist,double frontVal,double rdu,uint8_t rdu_dist){
+  _currentDebounceTime = millis();
  _edge_load_dist = edge_load_dist;
  _rdu = rdu;
  _rdu_dist = rdu_dist;
  _frontVal = frontVal; 
-  _isloaded = 0;
   _frontAng = edge_load_dist;
-  // if(_counter2 >= counter2_limit)
-  //   _counter2 = counter2_limit;
+  _isloaded = 1;
 }
 
 bool LoaderV1::isloaded() {
+
   return _isloaded;
 }
 
 void LoaderV1::reset() {
-  _counter = 0;
   setServo(_init_value);
 }
 
-// int LoaderV1::detect_LoadDist(){
-//   if (digitalRead(_buttonPin)==1){
-//     _edge_load_dist -= 2;
-//   }
-//  return _edge_load_dist;
-// }
-
 void LoaderV1::update() {
-  unsigned long currentDebounceTime = millis();
   if(_isloaded == 0) {
     if (digitalRead(_buttonPin) == 0 && _frontAng <130 && _frontAng>30) {
       setServo(_frontAng); //going front
        _frontAng -= _frontVal;
-       //_counter2 += change;
     } else  if(digitalRead(_buttonPin) == 1){
-      if (lastDebounceTime - currentDebounceTime >= 50){
+    //  if (millis() - _currentDebounceTime >= 65){
       _LoadFlag = 0;
-      //Serial.print(_rdu_dist);
-      setServo(_rdu_dist); //going back  
+      setServo(_rdu_dist);
+      delay(100); //going back 
       _rdu_dist-=_rdu;
-    //  ++_counter; 
-       _isloaded = 1;
-        currentDebounceTime = lastDebounceTime; 
-      }
-    }
-
-    // if (_rdu_dist >= rdu_limit) {
-    //   _counter = rdu;
-    // }
-  }
+     _isloaded = 1;
+     Serial.print("update");
+     Serial.print("\t");
+     Serial.println(_rdu_dist);
+      //}
+   }
 }
-
+}
 
 void LoaderV1::setServo(uint8_t angle){
    if(angle < 25){
@@ -66,6 +51,5 @@ void LoaderV1::setServo(uint8_t angle){
    if(angle>130){
     angle =130;
    } 
-   Serial.println(angle);
-   write(angle);//normal Servo.write
+      write(angle);//normal Servo.write
 }
