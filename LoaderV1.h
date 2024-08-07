dedce6a8d3e526ebdbf3f4d64ca7b0ba3fa7bc55
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-08-07 11:36:26 +0800

    Update LoaderV1.h
    Signed-off-by: NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>

--- LoaderV1.h
+++ LoaderV1.h
@@ -6,24 +6,24 @@
 class LoaderV1:public Servo {
 
 private:
-  uint8_t _init_value;
+  uint8_t _init_value = 120;
   uint8_t _servo_pin;
   uint8_t _buttonPin; 
-  uint8_t _rdu_dist;
-  uint8_t _edge_load_dist, _init_dist;
+  uint8_t _rdu_dist = 96;
+  uint8_t _edge_load_dist = 100;
   bool _isloaded = 1;
   bool _LoadFlag = 0;
-  double _rdu;  
-  double _frontVal;
+  double _rdu = 1;  
+  double _frontVal = 0.35;
   double _frontAng = _edge_load_dist;
   unsigned long _LastDelay = millis(),_CurrentDelay;
   unsigned long _currentDebounceTime = 0,_lastDebounceTime = millis();
-
+  void setServo(uint8_t angle); //Servo.write
 public:
-  LoaderV1(uint8_t servo_pin, uint8_t buttonPin, uint8_t init_value,uint8_t edge_load_dist,double frontVal,double rdu,uint8_t rdu_dist);
-  void load();
-  bool isloaded();
-  void setServo(uint8_t angle);
-  void reset();
-  void update();  
+  LoaderV1(uint8_t servo_pin, uint8_t buttonPin);
+  void setInitAng(uint8_t init_value, uint8_t edge_load_dist, double frontVal, double rdu, uint8_t rdu_dist);
+  void load(); //loading 1 ring
+  bool isloaded(); //returns value of isloaded(if the loading operation is complete or not) 
+  void reset(); //reset to init_angle(not required at init)
+  void update();  //loop :)
 };
