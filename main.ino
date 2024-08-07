403bf63f120f5337503727ac8626bb93de3ce00c
NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>
2024-08-07 11:37:42 +0800

    Update main.ino
    Signed-off-by: NicholasFSS161003 <164824012+NicholasFSS161003@users.noreply.github.com>

--- main.ino
+++ main.ino
@@ -3,30 +3,29 @@
 
 #define button 2
 
-bool load_flag = 0 ;
+bool load_flag = 0;
 bool press_flag = 0;
 
-LoaderV1 loader(9, 7, 100,70, 1.5, 2, 80);
+LoaderV1* loader;
 
 void setup() {
   Serial.begin(9600);
-  loader.attach(9);
-  loader.reset();
+  
+  loader = &LoaderV1(9, 7);
+
   pinMode(button, INPUT_PULLUP);
-  digitalWrite(button, HIGH);
-  }
+}
 
 void loop() {
-  loader.update();
-  if (digitalRead(button) == 0 && press_flag == 0) {
-    press_flag = 1; 
-     loader.load();
+  loader->update();
 
-  } else if (digitalRead(button) == 1 && press_flag == 1) {
-    press_flag = 0;
+  if (loader->isloaded() == 1) {
+    if (digitalRead(button) == 0 && press_flag == 0) {
+      press_flag = 1;  // Instant Debounce
+      loader->load();
+    } else if (digitalRead(button) == 1 && press_flag == 1) {
+      press_flag = 0;
+    }
   }
-  //Serial.print("Loader status1:  ");
-  //Serial.println(loader.isloaded());
-  //Serial.println(digitalRead(2));
 }
- 
+
